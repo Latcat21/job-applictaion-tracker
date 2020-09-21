91e3e83fd96ccd69ab259fa1618b8b0c437453f6
Brian <Brian@Brians-MacBook-Air.local>
2020-09-21 12:52:51 -0500

    setup templates

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -2,8 +2,25 @@ class ApplicationController < Sinatra::Base
   require 'bundler'
   Bundler.require()
 
+  #Directing to views folder
+  set :views, File.expand_path('../../views', __FILE__)
+
   get '/' do
     return 'hello'
+  end
+
+  #pry route for testing
+  get '/test' do
+    some_text = "test text variable"
+
+    binding.pry
+
+    "pry has finished -- here's some_text #{some_text}"
+
+  end
+
+  get '/hello' do
+    erb :hello
   end
 
 end
